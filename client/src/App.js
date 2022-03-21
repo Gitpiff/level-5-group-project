05d3826a6a48e989cdb208bd7a98eb7a756ec856
Deven McKay <deven_mckay@yahoo.com>
2022-03-21 09:25:44 -0400

    built DishSummary component / applied react logo files to fix missing file error/ constructed routes and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
+import React,{ useEffect, useState } from "react"
 import Nav from "./components/Nav"
 import Home from "./Home"
 import Search from "./Search"
 import { Routes, Route } from "react-router-dom"
 import Error from "./components/Error"
+import axios from "axios"
+import staticData from "./components/staticData"
 
 
 function App() {
+  const [dishes, setDishes] = useState(staticData)
+  const [isChef, setIsChef] = useState(false)
+  const [selectDish, setSelectDish] = useState()
+
+  useEffect(() => {
+    // axios.get({staticData})
+    //   .then(res => setDishes(res.data))
+    //   .catch(err => console.log(err))
+  }, [])
+
   return (
     <>
       <Nav />
       <div className='background-image'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home setIsChef={setIsChef}/>} />
+          <Route path="/search" element={<Search dishes={dishes} isChef={isChef}/>} />
           <Route path="*" element={(<><Error /><Home /></>)} />
         </Routes>
       </div>
--- client/src/Home.js
+++ client/src/Home.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom"
 import ChefLogo from "./components/images/chef.png"
 import CookLogo from "./components/images/cook.png"
 
-function Home() {
+function Home(props) {
+
+
   return (
     <div className="home">
-      <Link to="/search" >
-        <img src={CookLogo} alt="cook button logo"></img>
+      <Link to="/search" onClick={() => props.setIsChef(false)}>
+        <img src={CookLogo} alt="cook button logo" ></img>
       </Link>
-      <Link to="/search" >
-        <img src={ChefLogo} alt="chef button logo"></img>
+      <Link to="/search" onClick={() => props.setIsChef(true)}>
+        <img src={ChefLogo} alt="chef button logo" ></img>
       </Link>
     </div>
   )
